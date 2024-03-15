01cf98247b4db9116fc241f2138b703f19663fab
JORGEABARROCO <153916367+JORGEABARROCO@users.noreply.github.com>
2024-03-15 19:41:46 -0300

    Update TipoVeiculo.java

--- src/model/veiculo/TipoVeiculo.java
+++ src/model/veiculo/TipoVeiculo.java
@@ -1,16 +1,27 @@
 package model.veiculo;
 
+/**
+ * Esta enumeração representa os diferentes tipos de veículos.
+ */
 public enum TipoVeiculo {
-    PEQUENO(100),
-    MEDIO(150),
-    SUV(200);
+    PEQUENO(100),  // Representa um veículo de tamanho pequeno.
+    MEDIO(150),    // Representa um veículo de tamanho médio.
+    SUV(200);      // Representa um veículo do tipo SUV.
 
-    private final int valor;
+    private final int valor;  // Valor associado ao tipo de veículo.
 
+    /**
+     * Construtor da enumeração TipoVeiculo.
+     * @param valor Valor a ser associado ao tipo de veículo.
+     */
     TipoVeiculo(int valor) {
         this.valor = valor;
     }
 
+    /**
+     * Este método retorna o valor associado ao tipo de veículo.
+     * @return O valor do tipo de veículo.
+     */
     public int getValor() {
         return valor;
     }
